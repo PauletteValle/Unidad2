7ed985d84ed58c310560e17200f5f9320e9b7c61
PauletteValle <143724538+PauletteValle@users.noreply.github.com>
2023-09-05 19:14:37 -0600

    Add files via upload
    Este trabajo es de las 5 tareas.

---
+++ NuevaClase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class NuevaClase {
+    
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(string[] args) {
+        //TODO code application logic here
+    String tarea1 = "Hacer ejercicios de matematicas";
+    String tarea2 = "Estudiar Ciancias basicas"; 
+    String tarea3 = "Hacer la invetigacion de programacion"; 
+    String tarea4 = "Estudiar para el examen de calculo";
+    String tarea5 = "Hacer un resumen";
+    
+  
+}
